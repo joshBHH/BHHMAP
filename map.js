e44ce3885815ecc1a48ce162c06055cab7b253ff
Buckeye Hunter Hub <josh@buckeyehunterhub.com>
2025-11-19 03:45:53 -0500

    Update map.js

--- map.js
+++ map.js
@@ -1,14 +1,11 @@
-export let map, drawnItems, trackLayer, markersLayer;
+export let map;
 
 export function initMap() {
   map = L.map('map').setView([40.4173, -82.9071], 7);
 
-  const hybrid = L.tileLayer('https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=YOUR_KEY_IF_YOU_WANT', { attribution: 'MapTiler' }).addTo(map);
-  // add your other basemaps here the same way
+  L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Esri World Imagery'
+  }).addTo(map);
 
-  drawnItems = new L.FeatureGroup().addTo(map);
-  trackLayer = L.polyline([], {color: '#6dbc5d', weight: 4}).addTo(map);
-  markersLayer = new L.FeatureGroup().addTo(map);
-
-  // your draw control, etc. (copy from original)
+  console.log("Map initialized – Buckeye Hunter Hub v2");
 }
