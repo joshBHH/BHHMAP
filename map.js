886b945ae5756c7c5ea3537d0874760a5c7b2dbd
Buckeye Hunter Hub <josh@buckeyehunterhub.com>
2025-11-19 19:17:27 -0500

    Create map.js

---
+++ map.js
@@ -0,0 +1,11 @@
+let map = null;
+
+export function initMap() {
+  map = L.map('map').setView([40.4173, -82.9071], 7);
+
+  L.tileLayer('https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=VLOZCnjQYBtgpZ3BXBK3', {
+    attribution: '&copy; MapTiler &copy; OpenStreetMap contributors'
+  }).addTo(map);
+
+  window.map = map;
+}
