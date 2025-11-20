581659a9e67e44348ede8e99561624bf18da9553
Buckeye Hunter Hub <josh@buckeyehunterhub.com>
2025-11-20 03:11:07 -0500

    Update map.js

--- map.js
+++ map.js
@@ -2,6 +2,8 @@ let map = null;
 
 export function initMap() {
   map = L.map('map').setView([40.4173, -82.9071], 7);
+
   L.tileLayer('https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=VLOZCnjQYBtgpZ3BXBK3', { attribution: '&copy; MapTiler' }).addTo(map);
+
   window.map = map;
 }
